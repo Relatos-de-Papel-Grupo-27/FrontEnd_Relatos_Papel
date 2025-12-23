906c855373c1fd23ab0fb8ab64bc4a66dd3c93f7
jzapatag <jaime.zapata184@comunidadunir.net>
2025-12-23 18:55:19 -0500

    feat: inicializacion proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Relatos de Papel</h1>
+        <p>Front end creado en React para Proyecto transversal Relatos de papel - MISSI Unir 2025-2026</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
+
